10dee85e1694350421ef683c1731da6c9dd8528c
Leonardo <LeonardoVolxy@gmail.com>
2024-04-21 20:39:31 -0300

    sending updates

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,21 +1,40 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 
+import { createUser } from '../../lib/appwrite'
+
 const SignUp = () => {
   const [form, setForm] = useState({
     username: '',
+    email: '',
     password: ''
   })
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields!')
+    }
+
+    setIsSubmitting(true)
+    createUser();
+
+    try {
+      const result = await createUser(form.email, form.password, form.username)
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
@@ -54,7 +73,7 @@ const SignUp = () => {
           />
 
           <CustomButton
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
